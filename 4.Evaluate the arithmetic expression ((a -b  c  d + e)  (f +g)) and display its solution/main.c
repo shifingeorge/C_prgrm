d3175a9a5a02bfe544c7f997a5ebae2dba651dc5
shifingeorge <75887575+shifingeorge@users.noreply.github.com>
2021-11-16 16:39:26 +0530

    Update main.c

--- 4.Evaluate the arithmetic expression ((a -b  c  d + e)  (f +g)) and display its solution/main.c
+++ 4.Evaluate the arithmetic expression ((a -b  c  d + e)  (f +g)) and display its solution/main.c
@@ -5,5 +5,5 @@ int main()
     printf("Enter the 7 num:\n");
     scanf("%d%d%d%d%d%d%d",&a,&b,&c,&d,&e,&f,&g);
     z = ((a - b/c * d + e)*(f + g));
-    printf("Solution the arithmetic expression ((a -b / c * d + e) * (f +g)) is : %d",z);
+    printf("Solution of the arithmetic expression ((a -b / c * d + e) * (f +g)) is : %d",z);
 }
